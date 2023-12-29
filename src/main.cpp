55d97b31ebdb12ab7117747d896a3d94c8187639
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-12-29 22:11:57 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "indexer.hpp"
+#include "types.hpp"
 #include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
@@ -6,6 +8,32 @@
 int add(int i, int j) { return i + j; }
 
 namespace py = pybind11;
+
+struct Node
+{
+    double length{0.0};
+};
+
+struct Edge
+{
+};
+
+struct DiGraph
+{
+
+  private:
+    unordered_map<int64_t, Node> nodes_;
+    unordered_map<int64_t, unordered_set<int64_t>> nexts_, prevs_;
+    unordered_map<std::pair<int64_t, int64_t>, Edge> edges_;
+    mutable nano_fmm::Indexer indexer;
+
+    using IndexMap = unordered_map<int64_t, int64_t>;
+    using DistanceMap = unordered_map<int64_t, double>;
+
+    void single_source_upperbound_dijkstra(int64_t source, double distance, //
+                                           IndexMap &predecessor_map,
+                                           DistanceMap &distance_map) const;
+};
 
 PYBIND11_MODULE(_core, m)
 {
